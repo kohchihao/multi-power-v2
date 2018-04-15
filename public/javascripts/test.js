a3bd19c6267224daccb06f9ff55d866e09c04a47
Glitch (glib-profit) <none>
2018-04-15 15:56:35 +0000

    ⛲️📰 Checkpoint ./public/javascripts/test.js:620942/52

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -9,7 +9,7 @@ function loadnewdata() {
 			$.ajax({
         type: 'GET',
         //contentType: 'application/json',
-        url: 'http://localhost:3000/18051',						
+        url: 'https://glib-profit.glitch.me/18051',						
         success: function(data) {
             console.log('success');
             console.log(data);
