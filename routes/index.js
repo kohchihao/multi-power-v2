260421533df442ed2ad559dee9e868aaf02fb57a
Marcus Koh <kohchihao@gmail.com>
2019-05-30 22:56:30 +0800

    index.js: Remove JTC buses API

--- routes/index.js
+++ routes/index.js
@@ -87,25 +87,25 @@ const getBusData = (busId, cb) => {
         bus.push(data)
       });
 
-      //cb(bus);
-
-      let url = 'http://baseride.com/routes/api/platformbusarrival/377577/?format=json';	
-      let options = {	
-        url: url,	
-        method: 'GET',	
-        json: true,
-      }	
-
-      return rp(options);
-    })
-    .then(function (res2) {
-      finalData = parseApiEndPoint(res2);
-      finalData.forEach(busObj => {
-        bus.push(busObj)
-      })
-
       cb(bus);
+
+      // let url = 'http://baseride.com/routes/api/platformbusarrival/377577/?format=json';	
+      // let options = {	
+      //   url: url,	
+      //   method: 'GET',	
+      //   json: true,
+      // }	
+
+      // return rp(options);
     })
+    // .then(function (res2) {
+    //   finalData = parseApiEndPoint(res2);
+    //   finalData.forEach(busObj => {
+    //     bus.push(busObj)
+    //   })
+
+    //   cb(bus);
+    // })
     .catch(function (err) {
       //err
     });
